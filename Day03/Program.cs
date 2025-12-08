9a49edeaf33a4495c48f15c6fe24f184db0cd13a
Ted Statham <tstatham@local.sequiter.com>
2025-12-08 15:37:47 -0700

    Finished Day 3 part 1, started on part 2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -9,22 +9,61 @@ namespace Day03
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Advent of Code 2025: Day 3");
-			var puzzleInputRaw = File.ReadAllText($"./PuzzleInput-{((args.Length > 0 && args[0].Trim().ToLower() == "test") ? "test" : "full")}.txt");
+			var puzzleInputRaw = File.ReadLines($"./PuzzleInput-{((args.Length > 0 && args[0].Trim().ToLower() == "test") ? "test" : "full")}.txt").ToList();
 
-			PartA();
-			PartB();
+			PartA(puzzleInputRaw);
+			PartB(puzzleInputRaw);
 		}
 
-		private static void PartA()
+		private static void PartA(List<string> puzzleInputRaw)
 		{
 			Console.WriteLine("\r\n**********");
 			Console.WriteLine("* Part A");
-		}
 
-		private static void PartB()
+            var joltages = new List<int>();
+
+            foreach (var batteryBank in puzzleInputRaw)
+            {
+                var batteryJoltagesInBank = batteryBank.ToCharArray();
+                int maxJoltageInBank = 0;
+
+                for (int i = 0; i < (batteryJoltagesInBank.Length - 1); i++)
+                {
+                    int currentJoltage = int.Parse($"{batteryJoltagesInBank[i]}{batteryJoltagesInBank[(i + 1)..].Max()}");
+
+                    if (currentJoltage > maxJoltageInBank)
+                        maxJoltageInBank = currentJoltage;
+
+                    if (maxJoltageInBank == 99)
+                        break;
+                }
+
+                joltages.Add(maxJoltageInBank);
+                Console.WriteLine($"** Max joltage in bank {batteryBank} is {maxJoltageInBank:N0}");
+            }
+
+            Console.WriteLine($"* Joltage total is: {joltages.Sum():N0}");
+        }
+
+		private static void PartB(List<string> puzzleInputRaw)
 		{
 			Console.WriteLine("\r\n**********");
 			Console.WriteLine("* Part B");
+
+            var joltages = new List<ulong>();
+
+            foreach (var batteryBank in puzzleInputRaw)
+            {
+                var batteryJoltagesInBank = batteryBank.ToCharArray().Select(b => byte.Parse($"{b}")).ToArray();
+                ulong maxJoltageInBank = 0;
+
+                for(int i = 0; i < ( batteryJoltagesInBank.Length - 12); i++)
+                {
+                    // take the character at i
+                    // if the next character is > current character, add 1 to i and continue
+                    // if the next character is <= current character, add current character to string, increment i, and continue
+                }
+            }
 		}
 	}
 }
