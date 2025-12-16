6ed773725349cde9b074bf5b6e2e9fe809c8f430
Ted Statham <tstatham@local.sequiter.com>
2025-12-16 11:17:42 -0700

    Day 5, part 1 completed

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -6,21 +6,47 @@ namespace Day05
 {
 	internal class Program
 	{
-		private static List<string> badSafetyManualUpdates = [];
+        internal class IngredientRange
+        {
+            public ulong Start { get; set; }
+            public ulong End { get; set; }
+        }
 
-		static void Main(string[] args)
+        static void Main(string[] args)
 		{
 			Console.WriteLine("Advent of Code 2025: Day 5");
 			var puzzleInputRaw = File.ReadLines($"./PuzzleInput-{((args.Length > 0 && args[0].Trim().ToLower() == "test") ? "test" : "full")}.txt").ToList();
 
-			PartA();
+            List<IngredientRange> ingredientRanges = [.. puzzleInputRaw
+                .Where(i => i.Contains('-'))
+                .Select(i => i.Split('-')).ToList()
+                .Select(a => new IngredientRange { Start = ulong.Parse(a[0]), End = ulong.Parse(a[1]) })];
+
+            List<ulong> ingredients = [.. puzzleInputRaw
+                .Where(i => !i.Contains('-') && i.Length > 0)
+                .Select(i => ulong.Parse(i))];
+
+			PartA(ingredientRanges, ingredients);
 			PartB();
 		}
 
-		private static void PartA()
+		private static void PartA(List<IngredientRange> IngredientRanges, List<ulong> Ingredients)
 		{
 			Console.WriteLine("\r\n**********");
 			Console.WriteLine("* Part A");
+
+            int freshIngredients = 0;
+
+            foreach (var ingredient in Ingredients)
+            {
+                if (IngredientRanges.Any(r => ingredient >= r.Start && ingredient <= r.End))
+                {
+                    Console.WriteLine($"*** Ingredient {ingredient} is fresh");
+                    freshIngredients++;
+                }
+            }
+
+            Console.WriteLine($"** There are {freshIngredients} fresh ingredients.");
 		}
 
 		private static void PartB()
