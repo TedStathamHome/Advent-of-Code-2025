5241cb09aaa441654bd8d8e50182c52eb6172281
Ted Statham <tstatham@local.sequiter.com>
2025-12-18 09:50:17 -0700

    Completed Day 6, part 1

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -11,15 +11,48 @@ namespace Day06
             Console.WriteLine("Advent of Code 2025: Day 6");
             var puzzleInputRaw = File.ReadLines($"./PuzzleInput-{((args.Length > 0 && args[0].Trim().ToLower() == "test") ? "test" : "full")}.txt").ToList();
 
-			PartA();
+            List<int[]> problemValues = [.. puzzleInputRaw
+                .Where(i => !i.Contains('*'))
+                .Select(i => i.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(i => int.Parse(i)).ToArray())];
+
+            string[] problemOperators = puzzleInputRaw[^1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+			PartA(problemValues, problemOperators);
 			PartB();
 		}
 
-		private static void PartA()
+		private static void PartA(List<int[]> problemValues, string[] problemOperators)
 		{
 			Console.WriteLine("\r\n**********");
 			Console.WriteLine("* Part A");
-		}
+
+            ulong grandTotal = 0;
+
+            for (int i = 0; i < problemOperators.Length; i++)
+            {
+                char problemOperator = problemOperators[i][0];
+                ulong problemSolution = (ulong)((problemOperator == '+') ? 0 : 1);
+                List<ulong> values = [.. problemValues.Select(v => (ulong)v[i])];
+
+                for (int j = 0; j < values.Count; j++)
+                {
+                    if (problemOperator == '+')
+                    {
+                        problemSolution += values[j];
+                    }
+                    else
+                    {
+                        problemSolution *= values[j];
+                    }
+                }
+
+                Console.WriteLine($"** Problem {i}'s {((problemOperator == '+') ? "sum" : "product")} is {problemSolution:N0}");
+                grandTotal += problemSolution;
+            }
+
+            Console.WriteLine($"*** Grand total is {grandTotal:N0}");
+        }
 
 		private static void PartB()
 		{
